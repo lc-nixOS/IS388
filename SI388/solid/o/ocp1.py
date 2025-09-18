c2724c12cbb86684dea22ba2b05f1805e43726e1
rahman <nix.os.lc@gmail.com>
2025-09-18 18:21:01 -0500

    feat: implement OCP with inheritance and polymorphism for tuition cost calculation

---
+++ SI388/solid/o/ocp1.py
@@ -0,0 +1,28 @@
+# Aplicar el OCP: Usaremos herencia y polimorfismo. Creamos una clase base y luego subclases para cada tipo de estudiante.
+class TipoEstudiante:
+    def calcular_costo(self):
+        pass
+
+
+class Pregrado(TipoEstudiante):
+    def calcular_costo(self):
+        return 1500
+
+
+class Maestria(TipoEstudiante):
+    def calcular_costo(self):
+        return 2500
+
+
+class Doctorado(TipoEstudiante):
+    def calcular_costo(self):
+        return 3500
+
+
+class CalculadorMatricula:
+    @staticmethod
+    def calcular_costo(tipo_estudiante: TipoEstudiante):
+        return tipo_estudiante.calcular_costo()
+
+ca=CalculadorMatricula()
+print(ca.calcular_costo(Doctorado()))
